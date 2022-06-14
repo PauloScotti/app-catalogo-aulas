f16537796b925ae8d0daf3fd94ee394ec6b5aa06
PauloScotti <paulo.scotti@hotmail.com>
2022-06-13 22:50:50 -0300

    alterando cors e incluindo abrir aular

--- pages/api/aula.ts
+++ pages/api/aula.ts
@@ -4,17 +4,19 @@ import { conectaMongoDB } from "../../middlewares/conectaMongoDB";
 import { AulasModel } from "../../models/AulasModel";
 import { politicaCORS } from "../../middlewares/politicaCORS";
 
-const aulasEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg> | any) => {
+const aulaEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg> | any) => {
 
     try{
         if(req.method === 'GET'){
-            const {id} = req?.query;
-                const aulasModulo = await AulasModel.find({"modulo": id}).sort({nome: 1});
-                if(!aulasModulo){
-                    return res.status(400).json({erro: 'Módulo não é válido'});
-                }
-                return res.status(200).json(aulasModulo);
 
+            if(req?.query?.id){
+                const aulasEncontrada = await AulasModel.findById(req?.query?.id);
+                if(!aulasEncontrada){
+                    return res.status(400).json({erro: 'Aula não encontrada'});
+                }
+                return res.status(200).json(aulasEncontrada);
+
+            }
         }
 
         return res.status(405).json({erro: 'Método informado não é válido'});
@@ -27,4 +29,4 @@ const aulasEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaP
     return res.status(400).json({erro: 'Não foi possível obter os módulos'});
 }
 
-export default politicaCORS(conectaMongoDB(aulasEndpoint));
+export default politicaCORS(conectaMongoDB(aulaEndpoint));
